b7461f144d415ff3266fa0b776092b81507b46de
drisyaathul <drisyak809@gmail.com>
2023-06-06 00:09:49 +0800

    Added: Maximum and Minimum Value of 5 random 3 digit values

--- program1.js
+++ program1.js
@@ -1,19 +1,32 @@
 //Write a program that reads 5 Random 3 Digit values and then outputs the minimum and the maximum value
 
-let a = Math.floor(Math.random()*999);
 let b = Math.floor(Math.random()*999);
 let c = Math.floor(Math.random()*999);
 let d = Math.floor(Math.random()*999);
 let e = Math.floor(Math.random()*999);
-console.log("You Entered : "+a+" , "+b+" , "+c+" , "+d+" , "+e);
-if(a>b && a>c && a>d && a>e) {
-    console.log("Maximum value is : "+a);
-}else if(b>c && b>d && b>e) {
+let f = Math.floor(Math.random()*999);
+console.log("You Entered : "+b+" , "+c+" , "+d+" , "+e+" , "+f);
+//Maximum Value
+if(b>c && b>d && b>e && b>f) {
     console.log("Maximum value is : "+b);
-}else if(c>d && c>e) {
+}else if(c>d && c>e && c>f) {
     console.log("Maximum value is : "+c);
-}else if(d>e) {
+}else if(d>e && d>f) {
     console.log("Maximum value is : "+d);
-}else {
+}else if(e>f) {
     console.log("Maximum value is : "+e);
+}else {
+    console.log("Maximum value is : "+f);
+}
+//Minimum Value
+if(b<c && b<d && b<e && b<f) {
+    console.log("Minimum value is : "+b);
+}else if(c<d && c<e && c<f) {
+    console.log("Minimum value is : "+c);
+}else if(d<e && d<f) {
+    console.log("Minimum value is : "+d);
+}else if(e<f) {
+    console.log("Minimum value is : "+e);
+}else {
+    console.log("Minimum value is : "+f);
 }
