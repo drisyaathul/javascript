5c7977c8a731261d72e23cc216852ccce831de25
drisyaathul <drisyak809@gmail.com>
2023-06-05 23:29:39 +0800

    Added: Javascript basic Programs

---
+++ program1.js
@@ -0,0 +1,19 @@
+//Write a program that reads 5 Random 3 Digit values and then outputs the minimum and the maximum value
+
+let a = Math.floor(Math.random()*999);
+let b = Math.floor(Math.random()*999);
+let c = Math.floor(Math.random()*999);
+let d = Math.floor(Math.random()*999);
+let e = Math.floor(Math.random()*999);
+console.log("You Entered : "+a+" , "+b+" , "+c+" , "+d+" , "+e);
+if(a>b && a>c && a>d && a>e) {
+    console.log("Maximum value is : "+a);
+}else if(b>c && b>d && b>e) {
+    console.log("Maximum value is : "+b);
+}else if(c>d && c>e) {
+    console.log("Maximum value is : "+c);
+}else if(d>e) {
+    console.log("Maximum value is : "+d);
+}else {
+    console.log("Maximum value is : "+e);
+}
