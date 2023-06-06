f942874d99dff55b9c5ad8435692d50a92da455c
drisyaathul <drisyak809@gmail.com>
2023-06-07 00:44:58 +0800

    Added: Display the Days in Week Using Switch Case

---
+++ program9.js
@@ -0,0 +1,28 @@
+//Read a Number and Display the week day (Sunday, Monday,...) Using Case
+
+let day = "Thursday"
+
+switch(day){
+    case "monday":
+        console.log("Day is Monday");
+        break;
+    case "Tuesday":
+        console.log("Day is Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Day is Wednesday");
+        break;
+    case "Thursday":
+        console.log("Day is Thursday");
+        break;
+    case "Friday":
+            console.log("Day is Friday");
+            break;
+    case "Saturday":
+            console.log("Day is Saturday"); 
+            break;
+    default:
+        console.log("Weekend");
+        break;
+
+}
